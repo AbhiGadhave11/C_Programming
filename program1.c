841e831df5baeaefc6bd6e4fc52094501293454c
AbhiGadhave11 <91112733+AbhiGadhave11@users.noreply.github.com>
2021-10-30 11:43:27 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,15 @@
+// Display 5 numbers on screen
+// c   printf()
+// c++  cout
+
+#include<stdio.h>
+
+int main()
+{
+	printf("1\n");
+	printf("2\n");
+	printf("3\n");
+	printf("4\n");
+	printf("5\n");
+	return 0;
+}
